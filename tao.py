5aa6d6d1ba05c93bbfbc1c8acacb40aa1f1af9c6
XuanmoFeng <763608087@qq.com>
2017-08-24 20:52:50 +0800

    main

--- tao.py
+++ tao.py
@@ -3,22 +3,25 @@ import re
 import requests
 import sys
 sys.setdefaultencoding("utf-8")
-url = 'https://page.1688.com/html/homeIndex.html'
-print requests.get(url)
-payload = {'q': 'python','s': '1','ie':'utf8'}  #字典传递url参数    
-file = open('taobao_test.txt','w',encoding='utf-8')
+def TaoWeb(str):
+    url = 'https://page.1688.com/html/homeIndex.html'
+    print requests.get(url)
+    payload = {'q': str,'s': '1','ie':'utf8'}  #字典传递url参数    
+    file = open('taobao_test.txt','w',encoding='utf-8')
 
-for k in range(0,100):        #100次，就是100个页的商品数据
+    for k in range(0,100):        #100次，就是100个页的商品数据
 
-    payload ['s'] = 44*k+1   #此处改变的url参数为s，s为1时第一页，s为45是第二页，89时第三页以此类推                          
-    resp = requests.get(url, params = payload)
-    print(resp.url)          #打印访问的网址
-    resp.encoding = 'utf-8'  #设置编码
-    title = re.findall(r'"raw_title":"([^"]+)"',resp.text,re.I)  #正则保存所有raw_title的内容，这个是书名，下面是价格，地址
-    price = re.findall(r'"view_price":"([^"]+)"',resp.text,re.I)    
-    loc = re.findall(r'"item_loc":"([^"]+)"',resp.text,re.I)
-    x = len(title)           #每一页商品的数量
+        payload ['s'] = 44*k+1   #此处改变的url参数为s，s为1时第一页，s为45是第二页，89时第三页以此类推                          
+        resp = requests.get(url, params = payload)
+        print(resp.url)          #打印访问的网址
+        resp.encoding = 'utf-8'  #设置编码
+        title = re.findall(r'"raw_title":"([^"]+)"',resp.text,re.I)  #正则保存所有raw_title的内容，这个是书名，下面是价格，地址
+        price = re.findall(r'"view_price":"([^"]+)"',resp.text,re.I)    
+        loc = re.findall(r'"item_loc":"([^"]+)"',resp.text,re.I)
+        x = len(title)           #每一页商品的数量
 
-    for i in range(0,x) :    #把列表的数据保存到文件中
-        file.write(str(k*44+i+1)+'书名：'+title[i]+'\n'+'价格：'+price[i]+'\n'+'地址：'+loc[i]+'\n\n')
-file.close()
+        for i in range(0,x) :    #把列表的数据保存到文件中
+            file.write(str(k*44+i+1)+'书名：'+title[i]+'\n'+'价格：'+price[i]+'\n'+'地址：'+loc[i]+'\n\n')
+    file.close()
+if __name__=='__main__':
+    TaoWeb(小礼物)#爬取商品为小礼物的信息
